69b028ff80cc5d8d5cd41ae6c9c6687f39586158
Anastasia Silqa Lapteva <stasya.mango@gmail.com>
2023-03-16 00:23:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 485;
+        int mileCost = 20;
+
+        int rewards;
+        rewards = ticket / mileCost;
+        System.out.println(rewards);
+    }
+}
